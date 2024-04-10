09396dc47ca517df6fe570f268cb33ad6bc52a07
Nathan McCormack <G00414780@atu.ie>
2024-04-10 16:26:46 +0100

    Created add feature

---
+++ src/main/java/ie/atu/calc/Calculator.java
@@ -0,0 +1,10 @@
+package ie.atu.calc;
+
+public class Calculator {
+
+    //This takes in 2 numbers and adds them together
+    public int add(int first, int second){
+        return (first + second);
+    }
+
+}
